9698b1ddf2032567f7f2af342e9afc1b2fddda2f
Felix Angelov <felangelov@gmail.com>
2020-07-05 23:20:34 -0500

    docs: update examples/angular_counter to cubit ^0.2.0

--- examples/angular_counter/web/main.dart
+++ examples/angular_counter/web/main.dart
@@ -1,6 +1,16 @@
 import 'package:angular/angular.dart';
 import 'package:angular_counter/app_component.template.dart' as ng;
+import 'package:cubit/cubit.dart';
 
 void main() {
+  Cubit.observer = MyCubitObserver();
   runApp(ng.AppComponentNgFactory);
+}
+
+class MyCubitObserver extends CubitObserver {
+  @override
+  void onTransition(Cubit cubit, Transition transition) {
+    print(transition);
+    super.onTransition(cubit, transition);
+  }
 }
